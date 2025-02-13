538f83f3c30f74153c02342ac9c5c8e29ac4d929
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-02-12 22:00:37 -0300

    modificação do código

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ else if (xp >= 1001 && xp <= 2000) {
 else if (xp >= 2001 && xp <= 5000) {
     console.log(`O herói de nome ${herói} está no de nível Prata.`);
 }
-else if (xp >= 6001 && xp <= 7000) {
+else if (xp >= 5001 && xp <= 7000) {
     console.log(`O herói de nome ${herói} está no de nível Ouro.`);
 }
 else if (xp >= 7001 && xp <= 8000) {
